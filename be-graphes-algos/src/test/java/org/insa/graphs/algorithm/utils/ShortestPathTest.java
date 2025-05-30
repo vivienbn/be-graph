12796c1d0bde91adb99257d5c0a37f564995c936
vivienbn <vivienbn@yahoo.fr>
2025-05-30 15:35:51 +0100

    new test
    added a new test

--- be-graphes-algos/src/test/java/org/insa/graphs/algorithm/utils/ShortestPathTest.java
+++ be-graphes-algos/src/test/java/org/insa/graphs/algorithm/utils/ShortestPathTest.java
@@ -1,14 +1,11 @@
 package org.insa.graphs.algorithm.utils;
-
 import static org.junit.Assert.assertEquals;
-
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-
 import org.insa.graphs.algorithm.ArcInspectorFactory;
 import org.insa.graphs.algorithm.shortestpath.DijkstraAlgorithm;
 import org.insa.graphs.algorithm.shortestpath.ShortestPathData;
@@ -34,7 +31,7 @@ public class ShortestPathTest {
     private static final String TYPE_PATH_EXTENSION = ".path";
     private static final String TYPE_GRAPH_FILE_PATH = "../be-graphes-data-for-tests/map/";
     private static final String TYPE_GRAPH_EXTENSION = ".mapgr";
-    private static final String [] PATHS_NAME = {"BF_shortest_all_roads"};
+    private static final String [] PATHS_NAME = {"BF_shortest_all_roads", "BF_fastest_all_roads"};
 
     private static String getFileName(String name, FileType type) throws IOException{
         File mapFile = null;
@@ -91,5 +88,14 @@ public class ShortestPathTest {
         ShortestPathData data = new ShortestPathData(this.graph, origin, dest, ArcInspectorFactory.getAllFilters().get(0));
         DijkstraAlgorithm dijkstra = new DijkstraAlgorithm(data);
         assertEquals(dijkstra.run().getPath().getLength(), paths.get(0).getLength(), 0.0001);
+    }
+
+    @Test
+    public void testDijkstraFastest() {
+        Node dest = paths.get(1).getDestination();
+        Node origin = paths.get(1).getOrigin();
+        ShortestPathData data = new ShortestPathData(this.graph, origin, dest, ArcInspectorFactory.getAllFilters().get(2));
+        DijkstraAlgorithm dijkstra = new DijkstraAlgorithm(data);
+        assertEquals(dijkstra.run().getPath().getMinimumTravelTime(), paths.get(0).getMinimumTravelTime(), 0.0001);
     }
 }
